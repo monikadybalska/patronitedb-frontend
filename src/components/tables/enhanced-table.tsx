049a2ae976b703c6d6f56d9c8da46ce3d02eafb3
Monika <monika.dybalska@hotmail.com>
2024-06-10 16:05:54 +0200

    add material react table

---
+++ src/components/tables/enhanced-table.tsx
@@ -0,0 +1,123 @@
+import { Author } from "../../../lib/types";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { useMemo } from "react";
+
+import { fetchAllCategories } from "../../../lib/api";
+
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  type MRT_ColumnDef,
+} from "material-react-table";
+
+import { Link } from "@tanstack/react-router";
+
+export default function EnhancedTable() {
+  console.log("rerender");
+
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchAllCategories,
+  });
+
+  const columns = useMemo<
+    MRT_ColumnDef<
+      Author,
+      string | number | string[] | { name: string; image: string }
+    >[]
+  >(
+    () => [
+      {
+        accessorFn: (row) => row.name,
+        id: "name",
+        header: "Author",
+        Cell: ({ renderedCellValue, row }) => (
+          <>
+            <Link
+              to={`/authors/${renderedCellValue}`}
+              className="row-link"
+            ></Link>
+            <img src={row.original.image_url} className="row-image" />
+            <span className="title-cell">{renderedCellValue}</span>
+          </>
+        ),
+        sx: {
+          display: "flex",
+          alignItems: "center",
+          gap: "1rem",
+          padding: 0,
+        },
+      },
+      {
+        accessorFn: (row) => row.number_of_patrons,
+        id: "number_of_patrons",
+        header: "Total patrons",
+        filterVariant: "range-slider",
+        Cell: ({ cell }) => cell.getValue().toLocaleString("en-US"),
+      },
+      {
+        accessorFn: (row) => row.monthly_revenue,
+        id: "monthly_revenue",
+        header: "Monthly revenue",
+        filterVariant: "range-slider",
+        Cell: ({ cell }) => cell.getValue().toLocaleString("en-US"),
+      },
+      {
+        accessorFn: (row) => row.total_revenue,
+        id: "total_revenue",
+        header: "Total revenue",
+        filterVariant: "range-slider",
+        Cell: ({ cell }) => cell.getValue().toLocaleString("en-US"),
+      },
+      {
+        accessorFn: (row) => row.tags.split(","),
+        id: "tags",
+        header: "Tags",
+        filterVariant: "multi-select",
+        filterFn: "arrIncludesSome",
+        filterSelectOptions: categories,
+        Cell: ({ cell }) => cell.getValue<string[]>().join(", "),
+      },
+    ],
+    [categories]
+  );
+
+  const { data: serverData } = useQuery({
+    queryKey: ["chart"],
+    queryFn: async (): Promise<Author[]> => {
+      const response = await fetch(
+        `https://j1xfrdkw06.execute-api.eu-north-1.amazonaws.com/prod/top_authors?criteria=total_revenue&offset=0&limit=1000`
+      );
+      if (!response.ok) {
+        throw new Error("Network response error");
+      }
+      return response.json();
+    },
+  });
+
+  const data = useMemo(() => serverData ?? [], [serverData]);
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    initialState: { showColumnFilters: true },
+    enableFacetedValues: true,
+    muiTableBodyCellProps: ({ column }) => {
+      if (column.id === "name") {
+        return {
+          sx: {
+            display: "flex",
+            alignItems: "center",
+            gap: "1rem",
+            padding: 0,
+            width: "100%",
+          },
+        };
+      } else return { sx: {} };
+    },
+  });
+
+  return <MaterialReactTable table={table} />;
+}
--- src/routes/charts.lazy.tsx
+++ src/routes/charts.lazy.tsx
@@ -1,13 +1,13 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import Header from "../components/header";
-import Table from "../components/tables/tanstack-table/table";
+import EnhancedTable from "../components/tables/enhanced-table";
 
 export const Route = createLazyFileRoute("/charts")({
   component: () => (
     <>
       <Header title="Charts" subtitle="Charts and top rankings" />
       <div className="content">
-        <Table />
+        <EnhancedTable />
       </div>
     </>
   ),
